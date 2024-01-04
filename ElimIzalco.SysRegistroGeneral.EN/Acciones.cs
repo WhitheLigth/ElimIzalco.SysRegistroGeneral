9efec777782d6c572841857c54cb9becea42c494
Hector Garcia <hg425823@gmail.com>
2024-01-04 11:12:20 -0600

    Incorporacion de Clase para las Acciones CRUD

---
+++ ElimIzalco.SysRegistroGeneral.EN/Acciones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElimIzalco.SysRegistroGeneral.EN
+{
+    public class Acciones
+    {
+        public enum AccionEnum
+        {
+            Crear = 1,
+            Modificar = 2,
+            Eliminar = 3,
+            Ver = 4
+        }
+    }
+}
