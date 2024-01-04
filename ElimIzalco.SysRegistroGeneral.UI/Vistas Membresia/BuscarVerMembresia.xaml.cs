50b7aa7112135c3cc6afd0fade97fb62fc7c518a
Hector Garcia <hg425823@gmail.com>
2024-01-04 00:00:45 -0600

    Creacion de Vista BuscarVerMembresia junto con su documentacion

---
+++ ElimIzalco.SysRegistroGeneral.UI/Vistas Membresia/BuscarVerMembresia.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ElimIzalco.SysRegistroGeneral.UI.Vistas_Membresia
+{
+    /// <summary>
+    /// Lógica de interacción para BuscarVerMembresia.xaml
+    /// </summary>
+    public partial class BuscarVerMembresia : Page
+    {
+        public BuscarVerMembresia()
+        {
+            InitializeComponent();
+        }
+    }
+}
