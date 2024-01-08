7d1198f78b3b0adfad77de4bc6b14f4233ef9260
Hector Garcia <hg425823@gmail.com>
2024-01-07 23:31:39 -0600

    Integracion de Signo ? en los atributos de la clase MembresiEN

--- ElimIzalco.SysRegistroGeneral.EN/Membresia/MembresiaEN.cs
+++ ElimIzalco.SysRegistroGeneral.EN/Membresia/MembresiaEN.cs
@@ -6,6 +6,7 @@ using ElimIzalco.SysRegistroGeneral.EN.Estado_Civil;
 using ElimIzalco.SysRegistroGeneral.EN.Estatus;
 using ElimIzalco.SysRegistroGeneral.EN.Lista_de_Calendario;
 using ElimIzalco.SysRegistroGeneral.EN.Pastores;
+using ElimIzalco.SysRegistroGeneral.EN.Profesion_u_Oficio;
 using ElimIzalco.SysRegistroGeneral.EN.Sector;
 using ElimIzalco.SysRegistroGeneral.EN.Sexo;
 using ElimIzalco.SysRegistroGeneral.EN.Supervisores;
@@ -21,34 +22,35 @@ namespace ElimIzalco.SysRegistroGeneral.EN.Membresia
     public class MembresiaEN
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string Dui { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? Dui { get; set; }
         public DateTime FechaNacimiento { get; set; }
-        public string Edad {  get; set; }
-        public string Direccion { get; set; }
-        public string Telefono { get; set; }
-        public string LugarDeTrabajo { get; set; }
-        public string TelefonoDelTrabajo { get; set; }
+        public string? Edad {  get; set; }
+        public string? Direccion { get; set; }
+        public string? Telefono { get; set; }
+        public string? LugarDeTrabajo { get; set; }
+        public string? TelefonoDelTrabajo { get; set; }
         public DateTime FechaConversion {  get; set; }
-        public string LugarDeConversion { get; set; }
-        public string Digito { get; set; }
-        public string NombreLider {  get; set; }
-        public string Observaciones { get; set; }
+        public string? LugarDeConversion { get; set; }
+        public string? Digito { get; set; }
+        public string? NombreLider {  get; set; }
+        public string? Observaciones { get; set; }
         public DateTime FechaCreacion { get; set; }
         public DateTime FechaModificacion { get; set; }
         public byte[]? Fotografia { get; set; }
-        public SexoEN IdSexo {  get; set; }
-        public EstatusEN IdEstatus {  get; set; }
-        public EstadoCivilEN IdEstadoCivil {  get; set; }
-        public BautizmoEnAguaEN IdBautizmoEnAgua {  get; set; }
-        public BautizmoDelEspirituSantoEN IdBautizmoDelEspirituSanto {  get; set; }
-        public ListaDeCalendarioEN IdListaCalendario {  get; set; }
-        public PastoresEN  IdPastores {  get; set; }
-        public SupervisoresEN IdSupervisor {  get; set; }
-        public DistritoEN IdDistrito {  get; set; }
-        public ZonaEN IdZona {  get; set; }
-        public SectorEN IdSector {  get; set; }
-        public CelulaEN IdCelula {  get; set; }
+        public SexoEN? Sexo {  get; set; }
+        public EstatusEN? Estatus {  get; set; }
+        public EstadoCivilEN? EstadoCivil {  get; set; }
+        public BautizmoEnAguaEN? BautizmoEnAgua {  get; set; }
+        public BautizmoDelEspirituSantoEN? BautizmoDelEspirituSanto {  get; set; }
+        public ListaDeCalendarioEN? ListaCalendario {  get; set; }
+        public PastoresEN? Pastores {  get; set; }
+        public SupervisoresEN? Supervisor {  get; set; }
+        public DistritoEN? Distrito {  get; set; }
+        public ZonaEN? Zona {  get; set; }
+        public SectorEN? Sector {  get; set; }
+        public CelulaEN? Celula {  get; set; }
+        public ProfesionUOficioEN? ProfesionUOficio { get; set; }
     }
 }
