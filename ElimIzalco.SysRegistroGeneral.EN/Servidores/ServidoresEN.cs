3f5a07fe9c5dd4ae324ff912c69dfc995a89b894
Hector Garcia <hg425823@gmail.com>
2024-01-23 15:24:34 -0600

    Creacion de capa de entidad ServidoresEN

---
+++ ElimIzalco.SysRegistroGeneral.EN/Servidores/ServidoresEN.cs
@@ -0,0 +1,21 @@
+﻿using ElimIzalco.SysRegistroGeneral.EN.Estatus;
+using ElimIzalco.SysRegistroGeneral.EN.Membresia;
+using ElimIzalco.SysRegistroGeneral.EN.Privilegios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// REFERENCIAS NECESARIAS PARA EL CORRECTO FUNCIONAMIENTO
+
+
+namespace ElimIzalco.SysRegistroGeneral.EN.Servidores
+{
+    public class ServidoresEN
+    {
+        public int Id { get; set; }
+        public MembresiaEN Membresia { get; set; }
+        public PrivilegiosEN Privilegio { get; set; }
+        public EstatusEN Estatus { get; set; }
+    }
+}
