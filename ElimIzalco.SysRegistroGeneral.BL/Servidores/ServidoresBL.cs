1784cf56788db817c2f0c640edfe0c3263ee7e12
Hector Garcia <hg425823@gmail.com>
2024-01-24 16:44:05 -0600

    Incorporacion de Metodos Guardar, Modificar y Eliminar parala clase ServidoresBL

---
+++ ElimIzalco.SysRegistroGeneral.BL/Servidores/ServidoresBL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// REFERENCIAS NECESARIAS PARA EL CORRECTO FUNCIONAMIENTO
+using ElimIzalco.SysRegistroGeneral.EN.Supervisores;
+using ElimIzalco.SysRegistroGeneral.DAL.Servidores;
+using ElimIzalco.SysRegistroGeneral.EN.Servidores;
+
+namespace ElimIzalco.SysRegistroGeneral.BL.Servidores
+{
+    public class ServidoresBL
+    {
+        // Creamos una Instancia de la Clase SupervisoresDAL
+        ServidoresDAL ObjServidor = new ServidoresDAL();
+
+        // Metodo para Guardar una Nuevo Servidor
+        public int GuardarServidor(ServidoresEN pServidorGuardar)
+        {
+            return ObjServidor.GuardarServidor(pServidorGuardar);
+        }
+        // Merodo para Modificar un Servidor Existente
+        public int ModificarServidor(ServidoresEN pServidorModificar)
+        {
+            return ObjServidor.ModificarServidor(pServidorModificar);
+        }
+        // Metodo para Eliminar un Servidor Existente
+        public int EliminarServidor(ServidoresEN pServidorEliminar)
+        {
+            return ObjServidor.EliminarServidor(pServidorEliminar);
+        }
+    }
+}
