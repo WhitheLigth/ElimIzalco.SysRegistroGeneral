c90c2d7ea9f3d774bc1b716d7465384c98746487
Hector Garcia <hg425823@gmail.com>
2024-01-27 23:59:37 -0600

    Metodo para guardar en la BD, en la clase HistorialServidoresBL

---
+++ ElimIzalco.SysRegistroGeneral.BL/Historial Servidores/HistorialServidoresBL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// REFERENCIAS NECESARIAS PARA EL CORRECTO FUNCIONAMIENTO
+using ElimIzalco.SysRegistroGeneral.DAL.Historial_Servidores;
+using ElimIzalco.SysRegistroGeneral.EN.Historial_Servidores;
+
+
+namespace ElimIzalco.SysRegistroGeneral.BL.Historial_Servidores
+{
+    public class HistorialServidoresBL
+    {
+        // Creamos la Instancia para Acceder a los metodos
+        HistorialServidoresDAL ObjHistorialServidor  = new HistorialServidoresDAL();
+
+        // Metodo para Guardar un Nuevo Registro al Historial
+        public int GuardarHistorialServidor(HistorialServidoresEN pHistorialGuardar)
+        {
+            return ObjHistorialServidor.GuardarHistorialServidor(pHistorialGuardar);
+        }
+    }
+}
