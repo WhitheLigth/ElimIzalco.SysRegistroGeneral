c5d37bea7bd8bbb25a9a27a68a8d5968b419c010
Hector Garcia <hg425823@gmail.com>
2024-01-27 23:58:39 -0600

    New Clase HistorialServidoresEN

---
+++ ElimIzalco.SysRegistroGeneral.EN/Historial Servidores/HistorialServidoresEN.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// REFERENCIAS NECESARIAS PARA EL CORRECTO FUNCIONAMIENTO
+using ElimIzalco.SysRegistroGeneral.EN.Estatus;
+using ElimIzalco.SysRegistroGeneral.EN.Membresia;
+using ElimIzalco.SysRegistroGeneral.EN.Privilegios;
+
+
+namespace ElimIzalco.SysRegistroGeneral.EN.Historial_Servidores
+{
+    public class HistorialServidoresEN
+    {
+        public int Id { get; set; }
+        public MembresiaEN Membresia { get; set; }
+        public PrivilegiosEN Privilegio { get; set; }
+        public EstatusEN Estatus { get; set; }
+    }
+}
