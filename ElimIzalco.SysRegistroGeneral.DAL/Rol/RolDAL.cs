73ebfb8d1a9fc4f359cbd07e6c941441d5b479a1
Hector Garcia <hg425823@gmail.com>
2024-01-29 00:13:05 -0600

    Creacion de las Clases de Acceso a Datos RolDAL y UsuarioDAL

---
+++ ElimIzalco.SysRegistroGeneral.DAL/Rol/RolDAL.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+// REFERENCIAS NECESARIAS PARA EL CORRECTO FUNCIONAMIENTO
+using ElimIzalco.SysRegistroGeneral.EN.Rol;
+
+
+namespace ElimIzalco.SysRegistroGeneral.DAL.Rol
+{
+    public class RolDAL
+    {
+        #region Obtener Todos Los Roles
+        // Metodo para Obtener Todos los Roles Existentes
+        public List<RolEN> ObtenerRoles()
+        {
+            // Creamos una instancia de RolEN para acceder a los atributos
+            List<RolEN> listaRol = new List<RolEN>();
+
+            // Consulta hacia la Base de Datos
+            string consultaSQL = "SELECT Id, Nombre FROM Rol;";
+
+            SqlCommand command = ComunDB.ObtenerComando();
+
+            // Usar CommandType.Text para indicar que es una consulta directa en lugar de un procedimiento almacenado
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = consultaSQL;
+
+            SqlDataReader reader = ComunDB.EjecutarComandoReader(command);
+
+            while (reader.Read())
+            {
+                // Creamos una nueva instancia de RolEN para acceder a los atributos
+                RolEN Objrol = new RolEN();
+
+                // Asignacion de Columnas
+                Objrol.Id = reader.GetInt32(0);
+                Objrol.Nombre = reader.GetString(1);
+
+                // A los atributos de la primera instancia se le asignan los datos encontrados del ObjRol
+                listaRol.Add(Objrol);
+            }
+            // Retornamos el listado
+            return listaRol;
+        }
+        #endregion
+
+        #region Obtener Roles Por Id
+        // Metodo para Obtener Roles en Base al Id Proporcionado
+        public RolEN ObtenerRolesPorId(int? pId)
+        {
+            // Creamos una instancia de RolesEN para acceder a los atributos
+            RolEN Rol = new RolEN();
+
+            // Consulta hacia la Base de Datos
+            string consultaSQL = "SELECT Id, Nombre FROM Rol WHERE Id = @Id;";
+
+            SqlCommand command = ComunDB.ObtenerComando();
+
+            // Usar CommandType.Text para indicar que es una consulta directa en lugar de un procedimiento almacenado
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = consultaSQL;
+            command.Parameters.AddWithValue("@Id", pId);
+
+            SqlDataReader reader = ComunDB.EjecutarComandoReader(command);
+
+            if (reader.Read())
+            {
+                // Asignacion de columnas
+                Rol.Id = reader.GetInt32(0);
+                Rol.Nombre = reader.GetString(1);
+            }
+            // Retornamos el listado
+            return Rol;
+        }
+        #endregion
+
+    }
+}
---
+++ ElimIzalco.SysRegistroGeneral.DAL/Usuarios/UsuarioDAL.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+// REFEREMCIAS NECESARIAS PARA EL CORRECTO FUNCIONAMIENTO
+using ElimIzalco.SysRegistroGeneral.EN.Usuarios;
+using ElimIzalco.SysRegistroGeneral.EN.Rol;
+using System.Data.SqlClient;
+
+
+namespace ElimIzalco.SysRegistroGeneral.DAL.Usuarios
+{
+    public class UsuarioDAL
+    {
+        #region ObtenerUsuarios
+        // Metodo para Obtener todos los Usuarios Existentes
+        public List<UsuarioEN> ObtenerUsuarios()
+        {
+            // Creamos una instancia de UsuarioEn para acceder a los atributos
+            List<UsuarioEN> listaUsuarios = new List<UsuarioEN>();
+
+            // Consulta hacia la Base de Datos
+            string consultaSQL = "SELECT Users.Id, Users.Nombre, Users.Apellido, Users.Dui, Users.Correo, Users.Passw, Rol.Id, Rol.Nombre " +
+                "FROM Users JOIN Rol ON Users.IdRol = Rol.Id ;";
+
+            SqlCommand command = ComunDB.ObtenerComando();
+
+            // Usar CommandType.Text para indicar que es una consulta directa en lugar de un procedimiento almacenado
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = consultaSQL;
+
+            SqlDataReader reader = ComunDB.EjecutarComandoReader(command);
+
+            while (reader.Read())
+            {
+                // Creamos una nueva instancia de UsuarioEN para acceder a los atributos
+                UsuarioEN ObjUsuarios = new UsuarioEN();
+
+                // Asignacion de Columnas
+                ObjUsuarios.Id = reader.GetInt32(0);
+                ObjUsuarios.Nombre = reader.GetString(1);
+                ObjUsuarios.Apellido = reader.GetString(2);
+                ObjUsuarios.Dui = reader.GetString(3);
+                ObjUsuarios.Correo = reader.GetString(4);
+                ObjUsuarios.Password = reader.GetString(5);
+                ObjUsuarios.Rol = new RolEN
+                {
+                    Id = reader.GetInt32(6),
+                    Nombre = reader.GetString(7),
+                };
+                listaUsuarios.Add(ObjUsuarios);
+            }
+            // Retornamos la Lista de Usuarios
+            return listaUsuarios;
+        }
+        #endregion
+
+        #region ObtenerUsuariosPorId
+        // Metodo para Obtener un Usuario En base a su Id
+        public UsuarioEN ObtenerUsuarioPorId(int? pId)
+        {
+            // Creamos una instancia de UsuarioEN para acceder a los atributos
+            UsuarioEN ObjUsuarios = new UsuarioEN();
+
+            // Consulta hacia la Base de Datos
+            string consultaSQL = "SELECT Users.Id, Users.Nombre, Users.Apellido, Users.Dui, Users.Correo, Users.Passw, Rol.Id, Rol.Nombre  " +
+                "FROM Users JOIN Rol ON Users.IdRol = Rol.Id WHERE Users.Id = @Id";
+
+            SqlCommand command = ComunDB.ObtenerComando();
+
+            // Usar CommandType.Text para indicar que es una consulta directa en lugar de un procedimiento almacenado
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = consultaSQL;
+            command.Parameters.AddWithValue("@Id", pId);
+
+            SqlDataReader reader = ComunDB.EjecutarComandoReader(command);
+
+            if (reader.Read())
+            {
+                // Asignacion de Columnas
+                ObjUsuarios.Id = reader.GetInt32(0);
+                ObjUsuarios.Nombre = reader.GetString(1);
+                ObjUsuarios.Apellido = reader.GetString(2);
+                ObjUsuarios.Dui = reader.GetString(3);
+                ObjUsuarios.Correo = reader.GetString(4);
+                ObjUsuarios.Password = reader.GetString(5);
+                ObjUsuarios.Rol = new RolEN
+                {
+                    Id = reader.GetInt32(6),
+                    Nombre = reader.GetString(7),
+                };
+            }
+            return ObjUsuarios;
+        }
+        #endregion
+
+        #region ObtenerUsuarioLike
+        // Metodo para Obtener Usuarios que Coincida o se Parezca al parametro Nombre
+        public List<UsuarioEN> ObtenerUsuarioLike(string pNombre)
+        {
+            // Creamos una instancia de UsuarioEN para acceder a los atributos
+            List<UsuarioEN> listaUsuario = new List<UsuarioEN>();
+
+            // Consulta hacia la Base de Datos
+            string consultaSQL = "SELECT Users.Id, Users.Nombre, Users.Apellido, Users.Dui, Users.Correo, Users.Passw, Rol.Id, Rol.Nombre " +
+                "FROM Users JOIN Rol ON Users.IdRol = Rol.Id WHERE Users.Nombre = @Nombres";
+
+            SqlCommand command = ComunDB.ObtenerComando();
+
+            // Usar CommandType.Text para indicar que es una consulta directa en lugar de un procedimiento almacenado
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = consultaSQL;
+            command.Parameters.AddWithValue("@Nombre", "%" + pNombre + "%"); // Se Agregan caracteres comodín para buscar coincidencias parciales
+
+            SqlDataReader reader = ComunDB.EjecutarComandoReader(command);
+
+            while (reader.Read())
+            {
+                // Creamos una instancia de UsuarioEN para acceder a los atributos
+                UsuarioEN ObjUsuarios = new UsuarioEN();
+
+                // Asignacion de Columnas
+                ObjUsuarios.Id = reader.GetInt32(0);
+                ObjUsuarios.Nombre = reader.GetString(1);
+                ObjUsuarios.Apellido = reader.GetString(2);
+                ObjUsuarios.Dui = reader.GetString(3);
+                ObjUsuarios.Correo = reader.GetString(4);
+                ObjUsuarios.Password = reader.GetString(5);
+                ObjUsuarios.Rol = new RolEN
+                {
+                    Id = reader.GetInt32(6),
+                    Nombre = reader.GetString(7),
+                };
+                listaUsuario.Add(ObjUsuarios);
+            }
+            // Retornamos la Lista de Usuarios
+            return listaUsuario;
+        }
+        #endregion
+    }
+}
