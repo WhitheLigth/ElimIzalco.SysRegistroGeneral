740a1ceaace664c2b7302301cc860d2e1e92f9cf
Hector Garcia <hg425823@gmail.com>
2024-01-29 00:12:42 -0600

    Creacion de las Clases de Entidades RolEN y UsuarioEn

---
+++ ElimIzalco.SysRegistroGeneral.EN/Usuarios/UsuarioEN.cs
@@ -0,0 +1,22 @@
+﻿using ElimIzalco.SysRegistroGeneral.EN.Rol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// REFERENCIAS NECESARIAS PARA EL CORRECTO FUNCIONAMIENTO
+
+
+namespace ElimIzalco.SysRegistroGeneral.EN.Usuarios
+{
+    public class UsuarioEN
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? Dui {  get; set; }
+        public  string? Correo { get; set; }
+        public string? Password { get; set; }
+        public RolEN Rol {  get; set; }
+    }
+}
